9cc51013822016076a54e5802e869b28b76178d1
ThorbjoernJonsson <57418320+ThorbjoernJonsson@users.noreply.github.com>
2020-07-04 21:24:38 +0000

    Update GamePlay2.py

--- GamePlay2.py
+++ GamePlay2.py
@@ -1,4 +1,3 @@
-
 from graphics import *
 import time
 import random
@@ -48,15 +47,32 @@ btns = [Button("Deal", deal['x'], deal['y'], (0, 250, 0), deal['w'], deal['h']),
 Color_line = (255, 0, 0)
 y_place_line = int((deal['y'] + leave['y'])/2 + 22)
 font = pygame.font.Font('freesansbold.ttf', 18)
-text0 = font.render('Player 0', True, blue, grey)
-text1 = font.render('Player 1', True, blue, grey)
+text0 = font.render('Score 0', True, blue, grey)
+text1 = font.render('Score 0', True, blue, grey)
 textRect0 = text0.get_rect()
 textRect1 = text1.get_rect()
 textRect0.center = (deal['x']+40, y_place_line - 30)
 textRect1.center = (deal['x'] + 40, y_place_line + 30)
 
-def redrawWindow(win, game, p):
-    return 1
+def redrawWindow(win, deck, score):
+    win = pygame.display.set_mode((width, height))
+    win.fill(grey)
+    for btn in btns:
+        btn.draw(win)
+
+    pygame.draw.line(win, Color_line, (deal['x'], y_place_line), (deal['x'] + 1200, y_place_line), 4)
+    text0_new = font.render('Score ' + str(score[0]), True, blue, grey)
+    text1_new = font.render('Score ' + str(score[1]), True, blue, grey)
+    win.blit(text0_new, textRect0)
+    win.blit(text1_new, textRect1)
+    for i in range(len(deck)):
+        if deck[i][2] == 1:
+            img = pygame.image.load("cards_gif/b1fv.gif")
+            win.blit(img, deck[i][1])
+        elif deck[i][2] == 0:
+            img = pygame.image.load("cards_gif/" + deck[i][0] + ".gif")
+            win.blit(img, deck[i][1])
+    pygame.display.update()
 
 
 def main():
@@ -91,61 +107,58 @@ def main():
             print("Couldn't get game")
             break
 
+        if game.redraw:
+            if player == 0:
+                win = redrawWindow(win, game.deck_p1, game.score)
+            else:
+                win = redrawWindow(win, game.deck_p2, game.score)
+        if game.moves[0] != False and game.moves[1] != False:
+            if player == 0:
+                n.send('score')
+
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
 
-
-                if pos[0] >= game.deck_p1[0][1][0] and pos[0] <= game.deck_p1[0][1][0] + 70 and pos[1] >= game.deck_p1[0][1][1] and \
-                    pos[1] <= game.deck_p1[0][1][1] + 95:
-                    img = pygame.image.load("cards_gif/" + game.deck_p1[0][0] + ".gif")
-                    win.blit(img, (1000, y_place_line - 110))
-                    pygame.display.update()
-                    game.deck_p1[0][1] = (1000, y_place_line - 110)
-                    game.deck_p2[0][1] = (1000, y_place_line - 110)
-                    game.deck_p2[0][2] = False
-                    n.send(str(game.deck_p1[0][1][0]) + ','+ str(game.deck_p1[0][1][1]))
-                    #n.send(game.deck_p1)
-                    #data_string = pickle.dumps(str(game.deck_p1[0][1][0]) + ','+ str(game.deck_p1[0][1][1]))
-                    #print (data_string)
-                    #n.send(data_string)
-
-
-
-
+                for i in range(len(game.deck_p1)):
+                    if pos[0] >= game.deck_p1[i][1][0] and pos[0] <= game.deck_p1[i][1][0] + 70 and pos[1] >= game.deck_p1[i][1][1] and \
+                        pos[1] <= game.deck_p1[i][1][1] + 95:
+                        if player == 0:
+                            if game.deck_p1[i][2] == 0:
+                                if i == 17 or i == 19 or i == 21 or i == 23 or i == 25 or i == 27 or i == 29 or \
+                                    i == 31 or i == 33:
+                                    n.send("flip," + str(i-1))
+                                n.send(str(i) + "," + str(1000) + "," + str(y_place_line - 110))
+                                if (not game.moves[0]) and (not game.moves[1]):
+                                    n.send("first")
+                        else:
+                            if game.deck_p2[i][2] == 0:
+                                if i == 35 or i == 37 or i == 39 or i == 41 or i == 43 or i == 45 or i == 47 or \
+                                    i == 49 or i == 51:
+                                    n.send("flip," + str(i-1))
+                                n.send(str(i) + "," + str(1000) + "," + str(y_place_line + 30))
+                                if (not game.moves[0]) and (not game.moves[1]):
+                                    n.send("first")
 
                 if btns[0].click(pos):
                     if player == 0:
                         for i in range(52):
-                            if game.deck_p1[i][2]:
+                            if game.deck_p1[i][2] == 1:
                                 img = pygame.image.load("cards_gif/b1fv.gif")
-                            else:
+                            elif game.deck_p1[i][2] == 0:
                                 img = pygame.image.load("cards_gif/" + game.deck_p1[i][0] + ".gif")
-                            #img = pygame.transform.scale(img, (80, 80))
                             win.blit(img, game.deck_p1[i][1])
                     else:
                         for i in range(52):
-                            if game.deck_p2[i][2]:
+                            if game.deck_p2[i][2] == 1:
                                 img = pygame.image.load("cards_gif/b1fv.gif")
-                            else:
+                            elif game.deck_p2[i][2] == 0:
                                 img = pygame.image.load("cards_gif/" + game.deck_p2[i][0] + ".gif")
                             win.blit(img, game.deck_p2[i][1])
                     pygame.display.update()
                 elif btns[1].click(pos):
                     run = False
                     pygame.quit()
-                elif btns[2].click(pos) and game.connected():
-                        n.send('player 0')
-                elif btns[3].click(pos) and game.connected():
-                    print ('reupdate')
-                    for i in range(52):
-                        if game.deck_p2[i][2]:
-                            img = pygame.image.load("cards_gif/b1fv.gif")
-                        else:
-                            img = pygame.image.load("cards_gif/" + game.deck_p2[i][0] + ".gif")
-                        win.blit(img, game.deck_p2[i][1])
-                    pygame.display.update()
-    #time.sleep(15)
 
 if __name__ == "__main__":
     main()
