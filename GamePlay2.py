7ea11cf1785781789adce39ce3441ed4dc77c983
ThorbjoernJonsson <57418320+ThorbjoernJonsson@users.noreply.github.com>
2020-07-05 01:32:18 +0000

    Update GamePlay2.py

--- GamePlay2.py
+++ GamePlay2.py
@@ -1,12 +1,7 @@
-from graphics import *
-import time
-import random
-from Deck import*
-import time
 from network import Network
 import pygame
-import pickle
 pygame.font.init()
+import time
 
 
 class Button:
@@ -39,6 +34,7 @@ grey = (192, 192, 192)
 white = (255, 255, 255)
 green = (0, 255, 0)
 blue = (0, 0, 128)
+red = (255, 0, 0)
 deal = {'w': 120, 'h': 60, 'x':30, 'y':30}
 leave = {'w': 120, 'h': 60, 'x':30, 'y':550}
 btns = [Button("Deal", deal['x'], deal['y'], (0, 250, 0), deal['w'], deal['h']),
@@ -52,6 +48,8 @@ textRect0 = text0.get_rect()
 textRect1 = text1.get_rect()
 textRect0.center = (deal['x']+40, y_place_line - 30)
 textRect1.center = (deal['x'] + 40, y_place_line + 30)
+num_for_sort = {'h': 0, 's': 1, 'd': 2, 'c': 3}
+name_of_sorts = {'h': 'hearts', 's': 'spades', 'd': 'diamonds', 'c': 'clubs'}
 
 def redrawWindow(win, deck, score):
     win = pygame.display.set_mode((width, height))
@@ -71,6 +69,16 @@ def redrawWindow(win, deck, score):
         elif deck[i][2] == 0:
             img = pygame.image.load("cards_gif/" + deck[i][0] + ".gif")
             win.blit(img, deck[i][1])
+    pygame.display.update()
+
+def text_has_sort(card_sort):
+    win = pygame.display.set_mode((width, height))
+    win.fill(grey)
+    font = pygame.font.Font('freesansbold.ttf', 40)
+    text_have_sort = font.render('You have ' + name_of_sorts[card_sort] + '!', True, red, grey)
+    textRect_have_sort = text_have_sort.get_rect()
+    textRect_have_sort.center = (deal['x'] + 400, y_place_line)
+    win.blit(text_have_sort, textRect_have_sort)
     pygame.display.update()
 
 
@@ -122,22 +130,44 @@ def main():
                 for i in range(len(game.deck_p1)):
                     if pos[0] >= game.deck_p1[i][1][0] and pos[0] <= game.deck_p1[i][1][0] + 70 and pos[1] >= game.deck_p1[i][1][1] and \
                         pos[1] <= game.deck_p1[i][1][1] + 95:
+                        player_1_allowed = True
+                        player_2_allowed = True
                         if player == 0:
-                            if game.deck_p1[i][2] == 0:
-                                if i == 17 or i == 19 or i == 21 or i == 23 or i == 25 or i == 27 or i == 29 or \
-                                    i == 31 or i == 33:
-                                    n.send("flip," + str(i-1))
-                                n.send(str(i) + "," + str(1000) + "," + str(y_place_line - 110))
-                                if (not game.moves[0]) and (not game.moves[1]):
-                                    n.send("first")
+                            if game.deck_p1[i][2] == 0 and (i <= 7 or (i >= 16 and i <= 33)):
+                                player_1_sort = game.deck_p1[i][0][0]
+                                if game.moves[1]:
+                                    player_2_sort = game.moves[1][0]
+                                    if player_1_sort != player_2_sort and game.num_sorts_p1[num_for_sort[player_2_sort]] > 0:
+                                        player_1_allowed = False
+                                        text_has_sort(player_2_sort)
+                                        time.sleep(2)
+                                        win = redrawWindow(win, game.deck_p1, game.score)
+
+                                if player_1_allowed:
+                                    if i == 17 or i == 19 or i == 21 or i == 23 or i == 25 or i == 27 or i == 29 or \
+                                        i == 31 or i == 33:
+                                        n.send("flip," + str(i-1))
+                                    n.send(str(i) + "," + str(1000) + "," + str(y_place_line - 110))
+                                    if (not game.moves[0]) and (not game.moves[1]):
+                                        n.send("first")
                         else:
-                            if game.deck_p2[i][2] == 0:
-                                if i == 35 or i == 37 or i == 39 or i == 41 or i == 43 or i == 45 or i == 47 or \
-                                    i == 49 or i == 51:
-                                    n.send("flip," + str(i-1))
-                                n.send(str(i) + "," + str(1000) + "," + str(y_place_line + 30))
-                                if (not game.moves[0]) and (not game.moves[1]):
-                                    n.send("first")
+                            if game.deck_p2[i][2] == 0 and ((i >= 8 and i <= 15) or i >= 34):
+                                player_2_sort = game.deck_p2[i][0][0]
+                                if game.moves[0]:
+                                    player_1_sort = game.moves[0][0]
+                                    if player_1_sort != player_2_sort and game.num_sorts_p2[num_for_sort[player_1_sort]] > 0:
+                                        player_2_allowed = False
+                                        text_has_sort(player_1_sort)
+                                        time.sleep(2)
+                                        win = redrawWindow(win, game.deck_p2, game.score)
+
+                                if player_2_allowed:
+                                    if i == 35 or i == 37 or i == 39 or i == 41 or i == 43 or i == 45 or i == 47 or \
+                                        i == 49 or i == 51:
+                                        n.send("flip," + str(i-1))
+                                    n.send(str(i) + "," + str(1000) + "," + str(y_place_line + 30))
+                                    if (not game.moves[0]) and (not game.moves[1]):
+                                        n.send("first")
 
                 if btns[0].click(pos):
                     n.send("draw")
